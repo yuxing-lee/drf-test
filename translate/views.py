5a6ff95133d10d58d63cbdf65ca7a143f8b4f3dc
YUXING-LEE <leo832232@gmail.com>
2023-11-24 22:41:41 +0800

    refactor (translate): creating a 'Translate' class using the LSP、DIP concept

--- translate/views.py
+++ translate/views.py
@@ -2,19 +2,16 @@ from rest_framework import mixins, status, viewsets
 from rest_framework.response import Response
 
 from .models import Query
-from .serializers import QuerySerializer
-from .utils import translate_text
+from .serializers import GoogleTranslateSerializer
 
 
 class TranslateViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Query.objects.all()
-    serializer_class = QuerySerializer
+    serializer_class = GoogleTranslateSerializer
 
     def create(self, request):
-        translate_result = translate_text(request.data["source_text"],
-                                          request.data["target_language"])
-        serializer = self.serializer_class(data=translate_result)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        result = serializer.translate()
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(result, status=status.HTTP_201_CREATED)
